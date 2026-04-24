cda21f8a36fa9b7eaeaf1ae0ce15bc98513bbd57
sonia <soniamp95@gmail.com>
2026-04-24 18:50:36 +0200

    feat(create-project): install angular, eslint, prwttier and primeng

---
+++ my-journal-book/src/app/app.config.ts
@@ -0,0 +1,17 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import Lara from '@primeuix/themes/lara';
+import { routes } from './app.routes';
+import { providePrimeNG } from 'primeng/config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Lara,
+      },
+    }),
+  ],
+};
